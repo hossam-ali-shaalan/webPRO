5663920a962c6e9063e01a8fcb290e24a3423c9d
hossam ali <65079183+hossam-ali-shaalan@users.noreply.github.com>
2021-06-12 17:45:01 +0200

    Add files via upload

---
+++ j.js
@@ -0,0 +1,9 @@
+
+let landing=document.querySelector(".nava");
+imgsArray=["01.jpg","02.jpg","03.jpg","04.jpg"];
+setInterval(()=>{
+    x=Math.floor(Math.random()*4);
+landing.style.backgroundImage='url('+imgsArray[x]+')';
+},4000)
+
+
