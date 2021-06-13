0d038671fd7de382bfd13396a39fb351f7a10c0e
hossam ali <65079183+hossam-ali-shaalan@users.noreply.github.com>
2021-06-13 22:15:58 +0200

    Add files via upload

--- j2.js
+++ j2.js
@@ -1,27 +1,38 @@
 function validation(){
     var pass = document.getElementById("pass");
     var pass2 = document.getElementById("pass2");
-    var passs = document.getElementById("passs");
-    var passs2 = document.getElementById("passs2");
+    var passText = document.getElementById("passText");
+    var pass2Text = document.getElementById("pass2Text");
+    var phone = document.getElementById("phone");
+    var email = document.getElementById("email");
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var phone = "0123456789";
     var i,j;
     var checkUpper = false;
     var charCounter = 0;
     
-    passs.style.display = "block";
-    passs2.style.display = "block";
+    em.style.display = "block";
 
-    
-    for(i in pass.value){
-        if(pass.value[i] == ' '){
-            document.getElementById("passs").innerHTML="please write password so that it does not contain a space ";
+    if (!email.value.match(/^[a-zA-Z0-9]+\@[a-zA-Z]+\.[a-zA-Z]+$/)){
+        document.getElementById("em").innerHTML="please write correct email";
            return false;
-           }
     }
-    
-    
+    em.style.display = "none"; 
+
+    num.style.display = "block";
+
+    if (!phone.value.match(/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/)){
+        document.getElementById("num").innerHTML="please write the 'phone number' like this pattern<br>###-###-#### ";
+           return false;
+    }
+    num.style.display = "none";
+
+    passText.style.display = "block";
+    if(pass.value.search([' ']) != -1 ){
+        document.getElementById("passText").innerHTML="please write password so that it does not contain a space ";
+           return false;
+    }
+                
     for(j in uppercase){
          
         if(  pass.value[0] == uppercase[j] ){
@@ -30,44 +41,40 @@ function validation(){
     }
     }
     if(checkUpper == false ){
-        document.getElementById("passs").innerHTML="The password must start with uppercase character";
+        document.getElementById("passText").innerHTML="The password must start with uppercase character";
            return false;
     }
     
     
-    for(i in pass.value){
-        for(j in lowercase){
-            if(pass.value[i] == lowercase[j] || pass.value[i] == uppercase[j]){
-                charCounter = charCounter+1;
-            }
-        }
-    }
+
     
     
     if(pass.value.search(/[0-9]/) == -1){
-        document.getElementById("passs").innerHTML="The password must include 1 digit ";
+        document.getElementById("passText").innerHTML="The password must include 1 digit ";
         return false;
     }
    
     if(pass.value.search(/[!\@\#\$\%\^\&\*\(\)\_\-\+\=\{\}]/) == -1 ){
-        document.getElementById("passs").innerHTML="The password must include 1 special character like @ , $ ,... ";
+        document.getElementById("passText").innerHTML="The password must include 1 special character like @ , $ ,... ";
         return false;
     }
     
-    if(charCounter != 8){
-    document.getElementById("passs").innerHTML="the password must include 8 characters no more no less";
+    if(pass.value.length != 8){
+    document.getElementById("passText").innerHTML="the password must include 8 characters no more no less";
     return false;
     }
 
-    passs.style.display = "none";
+    passText.style.display = "none";
+
+    pass2Text.style.display = "block";
 
     if(pass.value != pass2.value){
-        
-        document.getElementById("passs2").innerHTML="Your password don't match. Please try again";
+        document.getElementById("pass2Text").innerHTML="Your password don't match. Please try again";
         return false;
     }
 
-    passs2.style.display = "none";
-    alert("Welcome to our libairy");
+    pass2Text.style.display = "none";
+alert("Welcome to the Cars world");
+   
     
 }
